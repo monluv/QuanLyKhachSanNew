ceb587f1efdf9235d42dede5db8d30135e2379cf
hdavu <hdavu.vn@gmail.com>
2017-06-05 10:23:33 +0700

    xong hiệp

--- QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmqlphong.Designer.cs
+++ QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmqlphong.Designer.cs
@@ -28,19 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenphong = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaphong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(643, 290);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 19;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(487, 290);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 18;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(349, 290);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 17;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(462, 180);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(100, 20);
+            this.txtGia.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Giá";
+            // 
+            // txtTenphong
+            // 
+            this.txtTenphong.Location = new System.Drawing.Point(462, 106);
+            this.txtTenphong.Name = "txtTenphong";
+            this.txtTenphong.Size = new System.Drawing.Size(100, 20);
+            this.txtTenphong.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Tên phòng";
+            // 
+            // txtMaphong
+            // 
+            this.txtMaphong.Location = new System.Drawing.Point(462, 39);
+            this.txtMaphong.Name = "txtMaphong";
+            this.txtMaphong.Size = new System.Drawing.Size(100, 20);
+            this.txtMaphong.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(387, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Mã phòng";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(294, 431);
+            this.dataGridView1.TabIndex = 10;
             // 
             // frmqlphong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(753, 455);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTenphong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaphong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "frmqlphong";
             this.Text = "qlphong";
+            this.Load += new System.EventHandler(this.frmqlphong_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenphong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaphong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmqlphong.cs
+++ QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmqlphong.cs
@@ -17,5 +17,120 @@ namespace QUANLYKHACHSAN2
         {
             InitializeComponent();
         }
+
+        SqlConnection con = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True");
+
+        private void hien()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * from phong", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            dataGridView1.DataSource = dt;
+
+            txtMaphong.DataBindings.Clear();
+            txtTenphong.DataBindings.Clear();
+            txtGia.DataBindings.Clear();
+            txtMaphong.DataBindings.Add("Text", dataGridView1.DataSource, "ma");
+            txtTenphong.DataBindings.Add("Text", dataGridView1.DataSource, "ten");
+            txtGia.DataBindings.Add("Text", dataGridView1.DataSource, "gia");
+        }
+
+        private void frmqlphong_Load(object sender, EventArgs e)
+        {
+            hien();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (txtMaphong.TextLength > 0 && txtTenphong.TextLength > 0 && txtGia.TextLength > 0)
+            {
+
+                if ((MessageBox.Show("Bạn muốn thêm phòng này?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+                {
+                    con.Open();
+
+                    string chuoisql = "insert into phong(ma,ten,gia) select N'{0}',N'{1}',N'{2}'";
+                    string them = string.Format(chuoisql, txtMaphong.Text, txtTenphong.Text, txtGia.Text);
+                    SqlCommand cmd = new SqlCommand(them, con);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Thêm thành công");
+                        con.Close();
+                        hien();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        con.Close();
+                    }
+                }
+                else
+                    hien();
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+            if (txtMaphong.TextLength > 0 && txtTenphong.TextLength > 0 && txtGia.TextLength > 0)
+            {
+
+                if ((MessageBox.Show("Bạn muốn sửa?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+                {
+                    con.Open();
+
+                    string chuoisql = string.Format("update phong set ten = N'{0}', gia = N'{1}' WHERE ma = N'{2}' ", txtTenphong.Text, txtGia.Text, txtMaphong.Text);
+                    SqlCommand cmd = new SqlCommand(chuoisql, con);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Sửa thành công");
+                        con.Close();
+                        hien();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        con.Close();
+                    }
+                }
+                else
+                    hien();
+            }
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Bạn muốn xóa thông tin?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                con.Open();
+                string sua = string.Format("delete from phong where ma={0}", txtMaphong.Text);
+                SqlCommand cmd = new SqlCommand(sua, con);
+                cmd.CommandType = CommandType.Text;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Xóa thành công");
+                    con.Close();
+                    hien();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    con.Close();
+                }
+            }
+
+        }
     }
 }
