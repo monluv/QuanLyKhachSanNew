66a1fedb5270d71fd7a513ca17840a0eb6c54272
Cong Dao <mmaimaiyeuemm@gmail.com>
2017-06-05 11:10:51 +0700

    tra phong- dao van cong

--- QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmtraphong.Designer.cs
+++ QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmtraphong.Designer.cs
@@ -28,19 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtTenkhach = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCMND = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaphong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(557, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "Ngày trả";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(624, 97);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(557, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Ngày thuê";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(624, 37);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 39;
+            // 
+            // txtTenkhach
+            // 
+            this.txtTenkhach.Location = new System.Drawing.Point(376, 167);
+            this.txtTenkhach.Name = "txtTenkhach";
+            this.txtTenkhach.Size = new System.Drawing.Size(124, 20);
+            this.txtTenkhach.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Tên khách";
+            // 
+            // txtCMND
+            // 
+            this.txtCMND.Location = new System.Drawing.Point(376, 98);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(124, 20);
+            this.txtCMND.TabIndex = 36;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "CMND";
+            // 
+            // txtMaphong
+            // 
+            this.txtMaphong.Location = new System.Drawing.Point(376, 34);
+            this.txtMaphong.Name = "txtMaphong";
+            this.txtMaphong.ReadOnly = true;
+            this.txtMaphong.Size = new System.Drawing.Size(124, 20);
+            this.txtMaphong.TabIndex = 34;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "mã phòng";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Trả";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(254, 377);
+            this.dataGridView1.TabIndex = 31;
             // 
             // frmtraphong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(850, 401);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtTenkhach);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaphong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "frmtraphong";
             this.Text = "trả phòng";
+            this.Load += new System.EventHandler(this.frmtraphong_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtTenkhach;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCMND;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaphong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmtraphong.cs
+++ QUANLYKHACHSAN2/QUANLYKHACHSAN2/frmtraphong.cs
@@ -17,5 +17,62 @@ namespace QUANLYKHACHSAN2
         {
             InitializeComponent();
         }
+        SqlConnection con = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=QuanLyKhachSan2;Integrated Security=True");
+        private void hien()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT ct.ma, ct.cmnd, k.ten, ct.ngaymuon, ct.ngaytra from chothue ct join khach k on k.cmnd=ct.cmnd where ct.ngaytra is NULL", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            dataGridView1.DataSource = dt;
+
+            txtMaphong.DataBindings.Clear();
+            txtCMND.DataBindings.Clear();
+            txtTenkhach.DataBindings.Clear();
+            dateTimePicker1.DataBindings.Clear();
+            dateTimePicker2.DataBindings.Clear();
+            txtMaphong.DataBindings.Add("Text", dataGridView1.DataSource, "ma");
+            txtCMND.DataBindings.Add("Text", dataGridView1.DataSource, "cmnd");
+            txtTenkhach.DataBindings.Add("Text", dataGridView1.DataSource, "ten");
+            dateTimePicker1.DataBindings.Add("Text", dataGridView1.DataSource, "ngaymuon");
+            dateTimePicker2.DataBindings.Add("Text", dataGridView1.DataSource, "ngaytra");
+
+        }
+
+        private void frmtraphong_Load(object sender, EventArgs e)
+        {
+            hien();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtMaphong.TextLength > 0 && txtCMND.TextLength > 0 && txtTenkhach.TextLength > 0)
+            {
+
+                if ((MessageBox.Show("Bạn muốn trả phòng?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+                {
+                    con.Open();
+
+                    string chuoisql = string.Format("update chothue set cmnd = N'{0}', ngaytra = N'{1}' WHERE phongma = N'{2}' ", txtCMND.Text, dateTimePicker2.Value.ToString("MM-dd-yyyy"), txtMaphong.Text);
+                    SqlCommand cmd = new SqlCommand(chuoisql, con);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Thành công");
+                        con.Close();
+                        hien();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        con.Close();
+                    }
+                }
+                else
+                    hien();
+            }
+        }
     }
 }
